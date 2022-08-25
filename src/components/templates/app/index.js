e40c2129ccb09625567b61f2db54a90d06abd2c7
Anirudh Sharma <anirudhsharma885@outlook.com>
2022-08-26 01:36:45 +0530

    Revert changes

--- src/components/templates/app/index.js
+++ src/components/templates/app/index.js
@@ -2,11 +2,9 @@ import React from "react";
 import Content from "../../layout/content-section/Content";
 import Header from "../../layout/header/Header";
 import Rating from "../../layout/rating/Rating";
-import Footer from "../../layout/footer/Footer";
+
 import Favicon from "react-favicon";
 import Section from "../../layout/section/Section";
-import FunFact from "../../layout/funFact/FunFact";
-import MainFooter from "../../layout/mainFooter/MainFooter";
 
 function ThemeOne({ page }) {
   console.log(page);
@@ -23,9 +21,6 @@ function ThemeOne({ page }) {
       <Content page={page} />
       <Rating page={page} />
       <Section page={page} />
-      <FunFact page={page} />
-      <Footer page={page} />
-      <MainFooter page={page} />
     </>
   );
 }
