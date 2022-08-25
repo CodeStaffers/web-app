3b4dc5ea889528b157e0fd80b40a701ba71746d1
Anirudh Sharma <anirudhsharma885@outlook.com>
2022-08-26 05:25:38 +0530

    authentication

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React, { useEffect, useState } from "react";
+import jwt_decode from "jwt-decode";
 import { client } from "./client";
 import "./App.css";
 import ThemeOne from "./components/templates/app/index";
 
+const clientId = process.env.REACT_APP_ACCESS_ID;
 function App() {
   const [page, setPage] = useState([]);
+  const [user, setUser] = useState({});
+  function handleCallbackResponse(response) {
+    var obj = jwt_decode(response.credential);
+    console.log(obj.exp);
+    setUser(obj);
+    document.getElementById("signInDiv").hidden = true;
+  }
 
   const getPage = async () => {
     try {
@@ -19,12 +28,42 @@ function App() {
     }
   };
   useEffect(() => {
+    /* global google */
+    google.accounts.id.initialize({
+      client_id: clientId,
+      callback: handleCallbackResponse,
+    });
+    google.accounts.id.prompt();
+    google.accounts.id.renderButton(document.getElementById("signInDiv"), {
+      theme: "outline",
+      size: "large",
+    });
     getPage();
   }, []);
 
   return (
     <div className="App1">
-      <ThemeOne page={page} />
+      {/*       
+    
+      {/* {Object.keys(user).length != 0 && (
+        <button onClick={(e) => handleSignOut(e)}>Sign Out</button>
+      )} */}
+
+      {Object.keys(user).length !== 0 ? (
+        <ThemeOne page={page} />
+      ) : (
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <div id="signInDiv"></div>
+          <h1>Welcome to CodeStaffers App</h1>
+        </div>
+      )}
     </div>
   );
 }
