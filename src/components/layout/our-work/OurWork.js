3b2d2f79302e8439475dcc5dae3c3a81c6002ee5
Anirudh Sharma <anirudhsharma885@outlook.com>
2022-09-19 19:09:36 +0530

    Section Change

--- src/components/layout/our-work/OurWork.js
+++ src/components/layout/our-work/OurWork.js
@@ -3,6 +3,7 @@ import { Button } from "../button/Button";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 import "./ourWork.styles.css";
 import { Link } from "react-router-dom";
+
 function OurWork({ page }) {
   return (
     <>
@@ -17,6 +18,9 @@ function OurWork({ page }) {
           <Link to={page.buttonLink}>
             <Button buttonSize="btn--large">{page.buttonText}</Button>
           </Link>
+        </div>
+        <div className="rectangle_ring">
+          <img src="/image/ring.png" alt="ring" />
         </div>
         <div className="ourwork-imagebox">
           <img src={page.bannerImage.fields.file.url} alt="banner" />
