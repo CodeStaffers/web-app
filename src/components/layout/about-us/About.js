5aa8aa3a26e168e8f13a69df3572992052ddd65a
md ashif reza <76996887+md-786910@users.noreply.github.com>
2022-10-03 17:19:26 +0530

    add contentfull to about page

--- src/components/layout/about-us/About.js
+++ src/components/layout/about-us/About.js
@@ -2,32 +2,41 @@ import React from "react";
 import "./about.styles.css";
 import { FcNext } from "react-icons/fc";
 import Divider from "../../Divider";
-function About() {
+
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+
+function About({ about }) {
+  const {
+    title,
+    contactUrl,
+    description,
+    happybgColor,
+    media,
+    mobileNo,
+    summary,
+    teamHappyDesc,
+    teamHappyTitle,
+    teamSummary,
+    teamTitle,
+    teamUrl,
+    titleStrong,
+  } = about;
+
   return (
     <>
       <section className="aboutSection about_container ">
         <div className="wrapper aboutWrapperDoc">
           <div className="aboutTitle aboutWrapper">
             <div className="aboutTitle1">
-              <h1>
-                We build bridges between
-                <span>companies and customers</span>
-              </h1>
+              {documentToReactComponents(title)}
             </div>
             <div className="aboutTitle2">
-              <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum
-                expedita reprehenderit debitis ducimus non odit dignissimos qui!
-                Vel, cumque necessitatibus.
-              </p>
+              {documentToReactComponents(summary)}
             </div>
           </div>
 
           <div className="aboutBg">
-            <img
-              src="https://images03.nicepage.com/c461c07a441a5d220e8feb1a/a6f4fa926896598d9b5a2e42/dmin.jpg"
-              alt="bg"
-            />
+            <img src={media.fields.file.url} alt="bg" />
           </div>
 
           <div className="aboutWrapper">
@@ -36,64 +45,46 @@ function About() {
 
           <div className="aboutSummary aboutWrapper">
             <div className="summaryTitle">
-              <h2>Together we are strong</h2>
+              <h2>{titleStrong}</h2>
             </div>
             <div className="summaryPara">
-              <h5>
-                Our Crew is always getting bigger, but we all work towards one
-                goal to make access not only possible but inevitable for teams
-                everywhere.
-              </h5>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam,
-                dolor reiciendis. Aperiam iste repellat, pariatur, minima fugit
-                sapiente dolore cum itaque aliquid consectetur nostrum voluptas
-                laborum dolor similique maxime. Porro perspiciatis nostrum
-                necessitatibus, aliquid ut, nesciunt quae possimus, enim neque
-                deserunt fugiat unde placeat aspernatur eius architecto
-                accusantium dolores expedita. Lorem ipsum dolor sit amet
-                consectetur adipisicing elit. Consequatur odit natus alias
-                inventore iusto reiciendis voluptatum vel aspernatur in iure
-                consequuntur exercitationem fuga earum, ratione magni, quos
-                velit, debitis expedita!
-              </p>
+              {documentToReactComponents(description)}
             </div>
           </div>
 
           <div className="aboutTeam aboutWrapper">
             <div className="team1">
-              <h2>Join our team</h2>
+              <h2>{teamTitle}</h2>
             </div>
             <div className="team2">
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis
-                temporibus eveniet, asperiores distinctio atque corrupti.
-              </p>
-              <a href="http">
+              {documentToReactComponents(teamSummary)}
+              <a href={teamUrl}>
                 See open position <FcNext />
               </a>
             </div>
           </div>
 
-          <div className="aboutAssit ">
+          <div
+            className="aboutAssit "
+            style={{ backgroundColor: { happybgColor } }}
+          >
             <div className="assist1 aboutWrapper">
-              <h2>Have a question?Our team is happy to assist you</h2>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Doloribus eos pariatur optio nemo maiores perspiciatis?
-              </p>
+              <h2>{teamHappyTitle}</h2>
+              <p>{teamHappyDesc}</p>
 
               <hr style={{ marginTop: "2.5em" }} />
 
               <div className="AboutBtn">
-                <button>
+                <button onClick={() => console.log(contactUrl)}>
                   Contact Us &nbsp; <FcNext />
                 </button>
 
                 <div>
                   Or call{" "}
-                  <span style={{ textDecoration: "underline" }}>
-                    +9132323232
+                  <span
+                    style={{ textDecoration: "underline", cursor: "pointer" }}
+                  >
+                    +91 {mobileNo}
                   </span>
                 </div>
               </div>
