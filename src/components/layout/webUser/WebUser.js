5fd188e9500a7372c710289e780e848c94110b3b
md ashif reza <76996887+md-786910@users.noreply.github.com>
2022-10-10 17:19:08 +0530

    add slider to brand user

--- src/components/layout/webUser/WebUser.js
+++ src/components/layout/webUser/WebUser.js
@@ -1,10 +1,23 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 
 import "./webUser.css";
 import { useSelector } from "react-redux";
 
-function WebUser({ topUser }) {
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Autoplay } from "swiper";
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import "swiper/css/scrollbar";
+import "./styleTop.css";
+
+function WebUser() {
+  const [swiper, setSwiper] = useState();
+  const [slide, setSlide] = useState(4);
+
   const [data, setData] = useState([]);
   const pageData = useSelector((state) => {
     return state.brand.brand;
@@ -20,6 +33,38 @@ function WebUser({ topUser }) {
     // eslint-disable-next-line
   }, []);
 
+  const prevRef = useRef();
+  const nextRef = useRef();
+
+  React.useEffect(() => {
+    if (swiper) {
+      swiper.params.navigation.prevEl = prevRef.current;
+      swiper.params.navigation.nextEl = nextRef.current;
+      swiper.navigation.init();
+      swiper.navigation.update();
+    }
+  }, [swiper]);
+
+  useEffect(() => {
+    function resizeWindow() {
+      const width = window.innerWidth;
+      if (width <= 815 && width >= 660) {
+        setSlide(3);
+      } else if (width <= 660 && width >= 425) {
+        setSlide(2);
+      } else if (width <= 425) {
+        setSlide(1);
+      } else {
+        setSlide(4);
+      }
+    }
+
+    window.addEventListener("resize", resizeWindow);
+
+    // remove listner
+    return () => window.removeEventListener("resize", resizeWindow);
+  }, []);
+
   const { title, media } = data;
 
   return (
@@ -33,12 +78,40 @@ function WebUser({ topUser }) {
             </div>
 
             <div className="webUserComp">
-              {media &&
-                media.map((item, index) => {
-                  return (
-                    <img src={item.fields.file.url} alt="comp" key={index} />
-                  );
-                })}
+              <Swiper
+                slidesPerView={slide}
+                spaceBetween={30}
+                slidesPerGroup={slide}
+                loop={true}
+                loopFillGroupWithBlank={true}
+                pagination={{
+                  clickable: true,
+                }}
+                autoplay={{
+                  delay: 3000,
+                  disableOnInteraction: false,
+                }}
+                modules={[Navigation, Pagination, Autoplay]}
+                navigation={{
+                  prevEl: prevRef?.current,
+                  nextEl: nextRef?.current,
+                }}
+                updateOnWindowResize
+                observer
+                observeParents
+                initialSlide={4}
+                onSwiper={setSwiper}
+                className="myTopSwiper"
+              >
+                {media &&
+                  media.map((item, index) => {
+                    return (
+                      <SwiperSlide key={index}>
+                        <img src={item.fields.file.url} alt="comp" />
+                      </SwiperSlide>
+                    );
+                  })}
+              </Swiper>
             </div>
           </div>
         </div>
