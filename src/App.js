5cfb465de8d2e1282d70788a4231979caf9a3699
Elite Web Solutions <111870642+elitewebsolutions@users.noreply.github.com>
2023-03-16 08:36:06 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,74 +1,18 @@
-import React, { useEffect, useState } from "react";
-import jwt_decode from "jwt-decode";
-import { client } from "./client";
-import ThemeOne from "./components/templates/app/index";
+import React from "react";
 import "./css/responsive.css";
-const clientId = process.env.REACT_APP_ACCESS_ID;
 
 function App() {
-  const [page, setPage] = useState([]);
-
-  const [user, setUser] = useState({});
-  function handleCallbackResponse(response) {
-    var obj = jwt_decode(response.credential);
-    console.log(obj.exp);
-    setUser(obj);
-    document.getElementById("signInDiv").hidden = true;
-  }
-
-  const getPage = async () => {
-    try {
-      const response = await client.getEntries({
-        content_type: "landingPage",
-      });
-      const responseData = response && response.items[0].fields;
-      if (responseData) setPage(responseData);
-    } catch (err) {
-      console.error(err);
-    }
-  };
-
-  useEffect(() => {
-    /* global google */
-    google.accounts.id.initialize({
-      client_id: clientId,
-      callback: handleCallbackResponse,
-    });
-    google.accounts.id.prompt();
-    google.accounts.id.renderButton(document.getElementById("signInDiv"), {
-      theme: "outline",
-      size: "large",
-    });
-    getPage();
-  }, []);
-
   return (
-    <div className="App1">
-      {/*       
-    
-      {/* {Object.keys(user).length != 0 && (
-        <button onClick={(e) => handleSignOut(e)}>Sign Out</button>
-      )} */}
-
-      {Object.keys(user).length !== 0 ? (
-        <ThemeOne page={page} />
-      ) : (
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            alignItems: "center",
-            height: "100vh",
-          }}
-          className="bg_home"
-        >
-          <div id="signInDiv"></div>
-          <div className="loginName">
-            <h1>Welcome to Elite Web Solutions</h1>
-          </div>
-        </div>
-      )}
+    <div
+      style={{
+        width: "100%",
+        height: "100%",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+      }}
+    >
+      <img src="/image/coming.jpg" alt="coming soon" />
     </div>
   );
 }
